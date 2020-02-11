072bdd47a2d661850f2cbd17699632aaba20d229
Sofia <sofia@proto366.haagahelia.amk>
2020-02-11 15:15:53 +0200

    added file to establish connection

---
+++ connect.php
@@ -0,0 +1,16 @@
+function connect_db() {
+ $servername = "localhost";
+ // EDIT HERE YOUR USERNAME AND PASSWORD
+ $username = "bgg293";
+ $password = "ceJE1Q99b";
+ $dbname = "cards";
+ // Create connection
+ $conn = new mysqli($servername, $username, $password, $dbname);
+ // Check connection
+ if ($conn->connect_error) {
+ die("Connection failed: " . $conn->connect_error);
+ } else {
+ }
+ return $conn;
+}
+?>
