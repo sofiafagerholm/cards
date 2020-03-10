bff001001a85a45c49493ed44ae263260ee4a1d4
Sofia <sofia@proto366.haagahelia.amk>
2020-03-10 13:42:43 +0200

    Revert "made a trash commit for revert demo"
    This reverts commit 848e7c1885b0a9e9af759474a21c19a1cf39bb08.

--- list.php
+++ list.php
@@ -7,7 +7,7 @@ if($_SESSION['username'] == 'username') {
  $sql = "SELECT * FROM Addresses";
  $result = mysqli_query($conn, $sql);
  if (mysqli_num_rows($result) > 0) {
- // output data of each row, revert practise
+ // output data of each row
  while($row = mysqli_fetch_assoc($result)) {
  echo $row["id"] . " ";
  echo $row["name"] . " ";
